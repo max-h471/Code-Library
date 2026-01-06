88304b37ef9fb08f3a5a8b470e3add22892d0d4a
max-h471 <maxwellhenderson423@gmail.com>
2026-01-06 11:00:59 -0500

    Update Asset_Tagging.py

--- Asset_Tagging.py
+++ Asset_Tagging.py
@@ -1,4 +1,4 @@
-# Script to make API calls to Rapid7
+# Script to make API calls to Rapid7, this one only has asset tagging API payloads, but the API call portion can effectively be used for any API call, but need to make new payloads
 import base64
 import urllib3
 import logging
@@ -19,25 +19,22 @@ base_host_url = "hxxps://hostname:consoleport.yourdomain.com"
 r7_username = "Your API User"
 r7_password = "Your API User Credentials"
 
-# header to be used in API request. R7 api requires base64 encoded creds
+# header to be used in API request. R7 API requires base64 encoded creds
 header = {
     "Authorization": "Basic " + base64.b64encode(f"{r7_username}:{r7_password}".encode('ascii')).decode(),
     "Accept": "application/json", "Content-Type": "application/json"
 }
 
 # Setup the logging for the script
-# logger module will return HTTP response codes
+# logger module will return HTTP response codes from the console server
 def setup_logging():
     logger = logging.getLogger('Rapid7')
     logger.setLevel(logging.INFO)
-    #
     console = logging.StreamHandler()
     console.setLevel(logging.INFO)
-    #
     formatter = logging.Formatter('%(asctime)s [%(levelname)8s] %(message)s')
     console.setFormatter(formatter)
     logger.addHandler(console)
-    #
     logger.info("Logging has been started...")
 
 # API call function with get, post, and put clauses for proper request handling
@@ -50,7 +47,7 @@ def api_call(method=None, data=None, url=None, ssl_verify=False):
         response = requests.get(headers=header, url=url, verify=ssl_verify)
         if response.status_code == 201 or response.status_code == 200:
             return response.text
-        # else clause to show error and exit script
+            # exit if something breaks and give servers response
         else:
             logging.error(f"Received: {response.status_code}, {response}, {url}, \n{response.request.headers}")
             raise SystemExit
@@ -59,12 +56,10 @@ def api_call(method=None, data=None, url=None, ssl_verify=False):
         response = requests.put(headers=header, json=data, url=url, verify=ssl_verify)
         return response.status_code, response.text
 
-# request body to hit the api URI at hxxps://host:port/api/3/tags
+# request body function to hit the api URI at hxxps://host:port/api/3/tags
 # this function will read hostnames from the text file and tag them 
 def CreateTag():
-    # enter an input file with hostnames
     status_code, response_body = api_call(method="post", url=base_host_url + "/api/3/tags")
-    # file input should be \n separated hostnames, NOT COMMA SEPARATED
     with open(base_file, 'r+') as input_file:
         # iterate through hosts in input file and use various hosts as multiple inputs
         lines = [line.strip() for line in input_file if line.strip()]
@@ -95,18 +90,16 @@ def CreateTag():
 
 # similar to the above function, but this one will tag single hostnames you define and not read from a text file
 def CreateTagSingle():
-    # enter an input file with hostnames
     status_code, response_body = api_call(method="post", url=base_host_url + "/api/3/tags")
 # API request body found at https://help.rapid7.com/insightvm/en-us/api/index.html#tag/Tag/operation/createTag
     api_args = {
-        # If your console rejects "default", remove this or use a hex color (e.g., "#0078D4")
         "color": "default",
         "name": "YOUR TAG NAME", # variable input, modify this
         "searchCriteria": {
             "filters": [
                 {
-                    "field": "host-name",    # same field you used in CreateTag()
-                    "operator": "contains",  # supported operator for host-name
+                    "field": "host-name",
+                    "operator": "contains",
                     "value": "YOUR ASSETS HOSTNAME" # variable input, modify this
                 }
             ],
@@ -124,8 +117,8 @@ def CreateTagSingle():
     logging.info(f"CreateTagSingle returned {status_code}: {response_body}")
     return status_code, response_body
 
+# main used to run whatever API function you are using
 def main():
-    # main used to run whatever API function you are using
     status_code, response_body = CreateTag() # uncomment if you are reading multiple hosts from a file
     #status_code, response_body = CreateTagSingle() # uncomment if you are doing a single host
     logging.getLogger('Rapid7').info("Script has completed.")
