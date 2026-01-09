043701ce81b7c0cf5c6b87fddfa1ce9faa9cc7aa
max-h471 <maxwellhenderson423@gmail.com>
2026-01-09 15:12:48 -0500

    Update Remove_OneStart.ps1
    refine script

--- PowerShell/Remove_OneStart.ps1
+++ PowerShell/Remove_OneStart.ps1
@@ -1,18 +1,24 @@
 # Onestart is a web browser and search engine that typically gets bundle-installed with other programs. It is a grayware/adware program that sets itself as the primary borwser and
 # exports various information about a user and their device. This script removes it. It is so common to be bundled with other software that I had to create a script to remove it
 
-# Kill OneStart processes
+# Kill OneStart and OneLaunch processes
 Taskkill /F /IM Onestart* /T
+Taskkill /F /IM Onelaunch* /T
 
-# Stop OneStart service if running
+# Stop OneLaunch service if running
 Stop-Service -Name "OneStart*" -ErrorAction SilentlyContinue
+Stop-Service -Name "OneLaunch*" -ErrorAction SilentlyContinue
 
-# Disable OneStart service
-Set-Service -Name "Onestart" -StartupType Disabled
+# Disable OneLaunch and OneStart service
+Set-Service -Name "Onestart*" -StartupType Disabled
+Set-Service -Name "OneLaunch*" -StartupType Disabled
 
-# Remove OneStart file system artifacts with * flag. Some folders are called onestart, some folders are called onestart.ai
-{ 
-$onestartpaths = @(
+# Remove scheduled task 
+schtasks.exe /delete /TN 'OneLaunchUpdateTask' /F
+schtasks.exe /delete /TN 'OneStartUpdateTask' /F
+
+# Remove OneStart and OneLaunch file system artifacts
+$OneStartPaths = @(
     "C:\Program Files\OneStart*",
     "C:\Program Files (x86)\OneStart*",
     "C:\Users\*\AppData\Local\OneStart*",
@@ -20,50 +26,80 @@ $onestartpaths = @(
     "C:\Users\*\OneStart.ai",
     "C:\Users\*\OneStart*",
     "C:\Users\*\Downloads\pdfguruhub.msi",
-    "C:\Users\*\Downloads\onestart*"
+    "C:\Users\*\Downloads\onestart*",
+    "C:\Program Files\Onelaunch*",
+    "C:\Program Files (x86)\Onelaunch*",
+    "C:\Users\*\AppData\Local\Onelaunch*",
+    "C:\Users\*\AppData\Roaming\Onelaunch*",
+    "C:\Users\*\Onelaunch.ai",
+    "C:\Users\*\Onelaunch*",
+    "C:\Users\*\Downloads\pdfguruhub.msi",
+    "C:\Users\*\Downloads\Onelaunch*"
 )
-foreach ($onestartpaths in $onestartpathss) {
-    Remove-Item -Path $onestartpaths -Recurse -Force -ErorAction SilentlyContinue}
+
+foreach ($path in $OneStartPaths) {
+    Get-ChildItem -Path $path -ErrorAction SilentlyContinue | ForEach-Object {
+        Write-Host "Deleting folder: $($_.FullName)"
+        Remove-Item -Path $_.FullName -Recurse -Force -ErrorAction SilentlyContinue
+    }
+}
+
+# Remove and Unregister OneLaunch DLLs
+$users = Get-ChildItem -Path "C:\Users" -Directory
+foreach ($user in $users) {
+    $OneLaunchPaths = "$($user.FullName)\AppData\Local\OneLaunch"
+    if (Test-Path -Path $OneLaunchPaths) {
+        $dlls = Get-ChildItem -Path "$OneLaunchPaths\*.dll"
+        foreach ($dll in $dlls) {
+            regsvr32 /u /s $dll.FullName
+        }
+
+    # Remove any OneLaunch*.exe files from Downloads folder if registry placed it back
+    $downloadsPath = "$($user.FullName)\Downloads\OneLaunch*.exe"
+    Remove-Item -Path $downloadsPath -Force -ErrorAction SilentlyContinue
+    }
 }
 
 # Remove OneStart executable and folder from all user profiles
 $users = Get-ChildItem -Path "C:\Users" -Directory
 foreach ($user in $users) {
-    $onestartpaths2 = "$($user.FullName)\AppData\Local\Onestart"
-    if (Test-Path -Path $onestartpaths2) {
+    $OneLaunchPaths = "$($user.FullName)\AppData\Local\OneStart"
+    if (Test-Path -Path $OneStartPaths) {
         # Unregister all DLLs in the OneStart folder
-        $dlls = Get-ChildItem -Path "$onestartpaths2\*.dll"
+        $dlls = Get-ChildItem -Path "$OneStartPaths\*.dll"
         foreach ($dll in $dlls) {
             regsvr32 /u /s $dll.FullName
         }
-
-    # Remove any onestart files from Downloads folder
-    $downloadsPath = "$($user.FullName)\Downloads\onestart*.exe"
+    # Remove any OneLaunch*.exe files from Downloads folder
+    $downloadsPath = "$($user.FullName)\Downloads\OneStart*.exe"
     Remove-Item -Path $downloadsPath -Force -ErrorAction SilentlyContinue
-}
+    }
 }
 
+# Remove OneLaunch Reg Keys
+Remove-Item -Path "Registry::HKU64\*\{4947c51a-26a9-4ed0-9a7b-c21e5ae0e71a}_is1" -Recurse
+Remove-Item -Path "Registry::HKU\*\{4947c51a-26a9-4ed0-9a7b-c21e5ae0e71a}_is1" -Recurse
 
-# Remove OneStart registry artifacts
+# Remove OneLaunch Reg Keys
 $registryPaths = @(
-    "HKLM:\SOFTWARE\OneStart*",
-    "HKLM:\SOFTWARE\WOW6432Node\OneStart*",
-    "HKCU:\Software\OneStart*",
+    "HKLM:\SOFTWARE\OneStart*",
+    "HKLM:\SOFTWARE\WOW6432Node\OneStart*",
+    "HKCU:\Software\OneStart*",
     "HKEY_USERS\software\microsoft\windows\currentversion\run\OneStart*",
     "HKEY_CURRENT_USER\*\SOFTWARE\MICROSOFT\WINDOWS\CURRENTVERSION\RUN\OneStartChromium",
     "HKEY_CURRENT_USER\*\SOFTWARE\MICROSOFT\WINDOWS\CURRENTVERSION\RUN\OneStartUpdate",
     "HKEY_CURRENT_USER\SOFTWARE\MICROSOFT\WINDOWS\CURRENTVERSION\RUN\OneStartUpdate",
     "HKEY_CURRENT_USER\SOFTWARE\MICROSOFT\WINDOWS\CURRENTVERSION\RUN\OneStartChromium",
-    "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\OneStartUser\*
+    "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\OneStartUser\*"
 
 )
 foreach ($registryPath in $registryPaths) {
-    Remove-Item -Path $registryPath -Recurse -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path $registryPath -Recurse -Force -ErrorAction SilentlyContinue
 }
 
-
-# Fully delete the onestart service 
+# Remove OneLaunch and OneStart service
 sc.exe delete OneStart
+sc.exe delete OneLaunch
 
-Write-Output "OneStart has been uninstalled silently from all profiles,
-Downloads folders, and OneStart folders."
+Write-Output "OneLaunch and OneStart have been uninstalled silently from all profiles,
+Downloads folders, and OneStart and OneLaunch folders, and any DLLs have been unregistered."
